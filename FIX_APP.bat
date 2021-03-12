bac3f6faeb34c448d8af61d62aa6b9dfc1fc3d88
yosriSamali <37900698+yosriSamali@users.noreply.github.com>
2021-03-12 16:17:42 +0100

    First commit

---
+++ FIX_APP.bat
@@ -0,0 +1,3 @@
+cd "C:/Users/Yosri SAMAALI/Desktop/FixParser"
+start "iexplore.exe" http://localhost:5000/
+npm start
---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+const FIXParser = require('fixparser');
+const fixParser = new FIXParser.default();
+
+// set the view engine to ejs
+// app.set('view engine', require('ejs').renderFile)
+app.engine('html', require('ejs').renderFile);
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static(__dirname + '/public/'));
+
+app.get('/', (req, res) => {
+    res.render('index.html')
+})
+
+app.post('/add', (req, res) => {
+    const fix_message = fixParser.parse(req.body.name);
+    const fix_pairs = Array.from(fix_message[0].data);
+    var tag = fix_pairs[1].tag;
+    var value = fix_pairs[1].value;
+    // console.log(fix_pairs);
+    const userValue = {
+        tag : tag,
+        value : value,
+        size: fix_pairs.length,
+        fix_pairs: fix_pairs
+    }
+    res.render('mypage.html', {
+        user: userValue
+        })
+})
+
+app.listen(5000,function(){
+    
+    console.log('server running on port 5000');
+})
